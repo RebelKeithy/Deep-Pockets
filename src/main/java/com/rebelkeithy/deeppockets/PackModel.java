1a3e1ea66f5345372747e675798fad3abe876c85
Dryxen <jrayburn87@yahoo.com>
2017-06-09 23:53:29 +0200

    Revert "Fixed Rendering on Server"
    This reverts commit ccc5c10c463fb784b42fe1158c9df08cf543341c.

--- src/main/java/com/rebelkeithy/deeppockets/PackModel.java
+++ src/main/java/com/rebelkeithy/deeppockets/PackModel.java
@@ -1,6 +1,8 @@
 package com.rebelkeithy.deeppockets;
 
 import com.rebelkeithy.deeppockets.Items.ItemMiningPack;
+import com.rebelkeithy.deeppockets.Items.DeepPocketsItems;
+
 import net.minecraft.client.renderer.ItemMeshDefinition;
 import net.minecraft.client.renderer.block.model.ModelResourceLocation;
 import net.minecraft.item.ItemStack;
@@ -8,14 +10,27 @@ import net.minecraft.item.ItemStack;
 public class PackModel implements ItemMeshDefinition  {
 
 	@Override
-	public ModelResourceLocation getModelLocation(ItemStack stack){ 	
-		
+	public ModelResourceLocation getModelLocation(ItemStack stack) 
+	{
+		if(stack.getItem() == DeepPocketsItems.miningPack)
+		{
 			ItemMiningPack pack = (ItemMiningPack)stack.getItem();
 			
 			int items = pack.getTotalItems(stack);
-			int i = Math.min(1+items/128, 4);			
-			return new ModelResourceLocation(stack.getItem().getRegistryName()+""+ i, "inventory");
+			int i = Math.min(1+items/128, 4);
+			return new ModelResourceLocation("miningpack:miningpack" + i, "inventory");
+		}
+
+		if(stack.getItem() == DeepPocketsItems.advancedMiningPack)
+		{
+			ItemMiningPack pack = (ItemMiningPack)stack.getItem();
+			
+			int items = pack.getTotalItems(stack);
+			int i = Math.min(1+items/128, 4);
+			return new ModelResourceLocation("miningpack:advancedminingpack" + i, "inventory");
+		}
 		
+		return null;
 	}
 
 }
--- src/main/java/com/rebelkeithy/deeppockets/proxy/CommonProxy.java
+++ src/main/java/com/rebelkeithy/deeppockets/proxy/CommonProxy.java
@@ -1,18 +1,8 @@
 package com.rebelkeithy.deeppockets.proxy;
 
-import com.rebelkeithy.deeppockets.Items.DeepPocketsItems;
-
-import net.minecraftforge.fml.common.event.FMLInitializationEvent;
-import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
-
 public class CommonProxy 
 {
-	 public void preinit(FMLPreInitializationEvent e){
-		 DeepPocketsItems.commonPreInit();
-	 }
-	 public void init(FMLInitializationEvent e){
-		 
-	 }
-	    
-	    
+	public void registerRenderers()
+	{
+	}
 }
